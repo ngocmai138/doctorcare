690ac21ded48011d0c01b8ddc73a3626b1c4c309
ngocmai138 <nnm0891@gmail.com>
2024-07-21 09:53:51 +0700

    Initial commit

---
+++ doctorcare.sql
@@ -0,0 +1,3 @@
+drop database if exists `doctorcare`;
+create database if not exists `doctorcare`;
+
---
+++ src/main/java/com/asm3/prj321/doctorcare/DoctorcareApplication.java
@@ -0,0 +1,13 @@
+package com.asm3.prj321.doctorcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorcareApplication.class, args);
+	}
+
+}
